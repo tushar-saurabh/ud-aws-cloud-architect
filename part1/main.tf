7e9923d7bdf4f8258cd4ccc2d453437279e15f11
tushar-saurabh <tush.saurabh@gmail.com>
2021-09-15 15:09:52 +0530

    Part1
    T4 Deleted

--- part1/main.tf
+++ part1/main.tf
@@ -12,13 +12,3 @@ resource "aws_instance" "T2" {
     "Name" = "Udacity T2"
   }
 }
-
-
-resource "aws_instance" "M4" {
-  count = 2
-  ami = "ami-087c17d1fe0178315"
-  instance_type = "m4.large"
-  tags = {
-    "Name" = "Udacity M4"
-  }
-}
